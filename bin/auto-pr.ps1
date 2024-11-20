f46c62c5b41c890b799194e9ec9ac5e7caab9ea4
sodacris <wjxa20152015@gmail.com>
2024-11-20 10:05:57 +0800

    move loc

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,11 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "soda92/apps:main"
 )
 
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
+if (!$env:SCOOP_HOME)
+{ $env:SCOOP_HOME = Convert-Path (scoop prefix scoop)
+}
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
 & $autopr -Dir $dir -Upstream $Upstream @Args
